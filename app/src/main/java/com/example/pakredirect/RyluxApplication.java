4c2dee1035680b5254e37908f2478f25bec0cfaf
yusuijiang01-orz <yusuijiang01@gmail.com>
2026-08-31 10:49:47 +0800

    Check for app updates when RYLUX becomes active

---
+++ app/src/main/java/com/example/pakredirect/RyluxApplication.java
@@ -0,0 +1,28 @@
+package com.example.pakredirect;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+public class RyluxApplication extends Application {
+    private boolean updateChecked;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override public void onActivityResumed(Activity activity) {
+                if (!updateChecked && activity instanceof MainActivity) {
+                    updateChecked = true;
+                    AppUpdateChecker.check(activity);
+                }
+            }
+            @Override public void onActivityCreated(Activity activity, Bundle state) {}
+            @Override public void onActivityStarted(Activity activity) {}
+            @Override public void onActivityPaused(Activity activity) {}
+            @Override public void onActivityStopped(Activity activity) {}
+            @Override public void onActivitySaveInstanceState(Activity activity, Bundle state) {}
+            @Override public void onActivityDestroyed(Activity activity) {}
+        });
+    }
+}
